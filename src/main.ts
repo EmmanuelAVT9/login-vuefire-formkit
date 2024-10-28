82a8287aa419d6af8e00320b686a65cf2d3788e8
EmmanuelAVT9 <alexsoge@gmail.com>
2024-10-28 14:44:45 -0600

    feat: Added vue-router dependency and integrated it into main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,17 @@
 import { createApp } from 'vue'
+import { VueFire, VueFireAuth } from 'vuefire'
 import './style.css'
 import App from './App.vue'
+import { auth } from './firebase'
+import router from './router'
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(VueFire, {
+  firebaseApp: auth.app,
+  modules: [VueFireAuth()],
+});
+
+app.use(router); // Usar el router
+
+app.mount('#app');
